e20b42815c45eed8c38d0c3e1fd9049a63ebfbf1
Rob <robert.koskulics@me.com>
2017-11-20 10:43:50 -0500

    Buttons and points work

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,32 +1,62 @@
 $(document).ready(function() {
 	// Create the variables for the goal number and the crystal numbers
-	var goal = 0;
+	var goal = 1;
 	var crystal1 = 0;
 	var crystal2 = 0;
 	var crystal3 = 0;
 	var crystal4 = 0;
+	var wins = 0;
+	var losses = 0;
+	var pointsToGoal = 0;
 
-	// Create the function to generate random numbers within a range
-	var minNumber = 0;
-	var maxNumber = 0;
-
-	var randomNumber = randomNumberFromRange(minNumber, maxNumber);
-
-	function randomNumberFromRange(min,max)
-	{
-	    return Math.floor(Math.random()*(max-min+1)+min);
-	}
-
-	// console.log(randomNumberFromRange(1, 12));
+	
 
 	// Create a function for the game
 	function initializeGame() {
-		// Generate random numbers for each of the elements we need.
 
+		// Randomize variables
+		goal = Math.floor((Math.random() * 120) + 1);
+		crystal1 = Math.floor((Math.random() * 12) + 1);
+		crystal2 = Math.floor((Math.random() * 12) + 1);
+		crystal3 = Math.floor((Math.random() * 12) + 1);
+		crystal4 = Math.floor((Math.random() * 12) + 1);
 
-
+		console.log(goal, crystal1, crystal2, crystal3, crystal4);
 	};
+		// If the user clicks a button it adds the button's value to the pointsToGoal var
+		$("#crystal1").on("click", function(){
+			pointsToGoal = pointsToGoal + crystal1
+			console.log(pointsToGoal);
+		});
+		$("#crystal2").on("click", function(){
+			pointsToGoal = pointsToGoal + crystal2
+			console.log(pointsToGoal);
+		});
+		$("#crystal3").on("click", function(){
+			pointsToGoal = pointsToGoal + crystal3
+			console.log(pointsToGoal);
+		});
+		$("#crystal4").on("click", function(){
+			pointsToGoal = pointsToGoal + crystal4
+			console.log(pointsToGoal);
+		});
+	
+	if(pointsToGoal === goal) {
+		wins = wins + 1;
+		initializeGame();
+	}else if(pointsToGoal >= goal) {
+		losses = losses + 1;
+		initializeGame();
+	};
+		// Start the game with random variables
+	initializeGame();
+
+
+
+	
+	
 
 
 });
+
 
